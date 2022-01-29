3868e01a7e867858df8e12cc552e161640e93d6d
DESKTOP-EHV202J\abraham <gloechacon@gmail.com>
2022-01-29 18:42:13 -0300

    Programa sin clases definidas

--- Program.cs
+++ Program.cs
@@ -1,4 +1,4 @@
-﻿using System;
+using System;
 
 namespace ArregloDeNumeros
 {
@@ -6,7 +6,26 @@ namespace ArregloDeNumeros
     {
         static void Main(string[] args)
         {
+            int[] numeros = new int[] { 5, 8, 4, 10, 6, 22, 1, 7, 9, 2};
+            int i, j, aux;
+            for (i = 0; i < 10; i++)
+            {
+                for (j = 0; j<10; j++)
+                {
+                    if (numeros[i] < numeros[j])
+                    {
+                        aux = numeros[i];
+                        numeros[i] = numeros[j];
+                        numeros[j] = aux;
+                    }
 
+
+                }
+            }
+            foreach (int x in numeros)
+            {
+                Console.WriteLine(x);
+            }
         }
     }
 }
